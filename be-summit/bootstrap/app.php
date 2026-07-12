d54c189c1ced2877486c3bf2d0dfad0745fdc93b
devfajarr <bayu23404@gmail.com>
2026-07-12 14:05:41 +0700

    menambahkan $middleware->trustProxies(at: '*'); pada file bootstrap/app.php.

--- be-summit/bootstrap/app.php
+++ be-summit/bootstrap/app.php
@@ -16,9 +16,9 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -26,6 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => EnsureRole::class,
             'kyc.verified' => EnsureKycVerified::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // $exceptions->shouldRenderJsonWhen(
